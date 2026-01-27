ab020cbaa974604c3556f078541e6daa427a0333
luxeshadow <luxe9152@gmail.com>
2026-01-27 14:18:00 +0000

    fix: change whatsapp to Whatsapp

--- src/presentation/views/home/home_page.vue
+++ src/presentation/views/home/home_page.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import Navbar from '../../components/layouts/landing/NavBar.vue'
 import Footer from '../../components/layouts/landing/Footer.vue'
-import whatsapp from '../../components/layouts/landing/whatsapp.vue'
+import whatsapp from '../../components/layouts/landing/Whatsapp.vue'
 import Hero from '../../components/hero/Hero.vue'
 import CeoMessage from '../../components/ceo_message/CeoMessage.vue'
 import Team from '../../components/team/components/Team.vue'
